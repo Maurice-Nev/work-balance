b1482d88c53d7c109f3236a178d01800f0520c04
Maurice-Nev <150186404+Maurice-Nev@users.noreply.github.com>
2025-03-06 14:16:12 +0100

    disable RLS

---
+++ supabase/migrations/20250306131435_remote_schema.sql
@@ -0,0 +1,13 @@
+alter table "public"."department" disable row level security;
+
+alter table "public"."rating" disable row level security;
+
+alter table "public"."role" disable row level security;
+
+alter table "public"."session" disable row level security;
+
+alter table "public"."stress" disable row level security;
+
+alter table "public"."user" disable row level security;
+
+
